342ca1129365f7f2b15e3799d1b40d4f46ddd49d
Ali Osman Zengin <66180039+aliosmanzengin@users.noreply.github.com>
2021-12-28 12:59:31 +0300

    page objects

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -1,16 +1,19 @@
 ///<reference types="cypress" />
 
+import { TodoPage } from "../page-objects/todo-page"
+
 
 describe('todo actions',()=>{
+    const todoPage = new TodoPage()
+
     beforeEach( () => {
-        cy.visit('http://todomvc-app-for-testing.surge.sh/')
+        todoPage.navigate()
     
-        cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
+        todoPage.addTodo('Clean Room')
     })
-    it('should add a new todo to the list', () => {
-        
+    it('should add a new todo to the list', () => {  
     
-        cy.get('label').should('have.text', 'Clean room')
+        todoPage.validateTodoTxt(0,'Ckeab Room')
         cy.get('.toggle').should('not.be.checked')
         
     })
---
+++ cypress/page-objects/todo-page.js
@@ -0,0 +1,14 @@
+//in js files called as "module"
+export class TodoPage{//export is says that other classes can import that one
+    navigate(){
+        cy.visit('http://todomvc-app-for-testing.surge.sh/')
+    }
+
+    addTodo(todoText){
+        cy.get('.new-todo').type(todoText+"{enter}")
+    }
+
+    validateTodoTxt(todoIndex, expectedText){
+        cy.get(`.todo-list li:nth-child(${todoIndex+1}) label`).should('have.text', expectedText)
+    }
+}
